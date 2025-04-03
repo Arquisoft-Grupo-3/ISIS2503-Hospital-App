cd9ecfa43e701f5aade14bb27ddd52642275faf4
isalozanoron <i.lozano1@uniandes.edu.co>
2025-04-03 16:22:59 -0500

    Mas de 10

--- MRI/views.py
+++ MRI/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .forms import MRIForm
 from django.contrib import messages
 from django.http import HttpResponseRedirect
@@ -6,11 +7,13 @@ from django.urls import reverse
 from .logic.logic_MRI import create_mri, get_mris
 
 def MRI_list(request):
-    mris = get_mris()[:10] 
-    context = {
-        'MRI_list': mris
-    }
-    return render(request, 'MRI/MRI.html', context)
+    mris = get_mris()
+    paginator = Paginator(mris, 10)  
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'MRI/MRI.html', {'page_obj': page_obj})
 
 def MRI_create(request):
     if request.method == 'POST':
