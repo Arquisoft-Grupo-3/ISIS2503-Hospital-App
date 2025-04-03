71897515077b8aa613c034d79b7abbfd38f2ce11
isalozanoron <i.lozano1@uniandes.edu.co>
2025-04-03 11:02:12 -0500

    OLA

--- MRI/urls.py
+++ MRI/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('MRI/', views.MRI_list),
-    path('MRIcreate/', csrf_exempt(views.MRI_create), name='MRICreate'),
+    path('', views.MRI_list, name='MRIList'),
+    path('create/', views.MRI_create, name='MRICreate'),
 ]
